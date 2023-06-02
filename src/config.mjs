22e6d7f8ec0e7ae8d1f2387f65f5e75bae7c1782
Kazuto Kusama <jacopen@gmail.com>
2023-06-03 00:05:29 +0900

    一般社団法人の記述を追加

--- src/config.mjs
+++ src/config.mjs
@@ -1,13 +1,13 @@
-import defaultImage from './assets/images/default.png';
+import defaultImage from './assets/images/logo_horizontal.png';
 
 const CONFIG = {
-  name: 'クラウドネイティブイノベーターズ協会',
+  name: '一般社団法人クラウドネイティブイノベーターズ協会',
 
   origin: 'https://cnia.io',
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'クラウドネイティブイノベーターズ協会',
+  title: '一般社団法人クラウドネイティブイノベーターズ協会',
   description:
     'クラウドネイティブ技術の発展・推進を支援するための一般社団法人です。',
   defaultImage: defaultImage,
--- src/data.js
+++ src/data.js
@@ -33,6 +33,6 @@ export const footerData = {
     { ariaLabel: 'Twitter', icon: 'tabler:brand-twitter', href: '#' },
   ],
   footNote: `
-    Copyright &copy; クラウドネイティブイノベーターズ協会
+    Copyright &copy; 一般社団法人クラウドネイティブイノベーターズ協会
   `,
 };
