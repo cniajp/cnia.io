960d415c51837729fc06f7cebdf608c645d42740
tanayan299 <infuse130@gmail.com>
2024-02-08 17:18:33 +0900

    feat: add draft Sponsor section

--- src/pek2024_data.js
+++ src/pek2024_data.js
@@ -7,16 +7,16 @@ export const headerData = {
       href: '/pek2024',
     },
     {
+      text: 'Sessions',
+      href: '/pek2024/sessions',
+    },
+    {
       text: 'TimeTable',
       href: '/pek2024/timetable',
     },
     {
-      text: 'Sponsors',
-      href: '/pek2024/sponsors',
-    },
-    {
-      text: 'FAQ',
-      href: '/pek2024/faq',
+      text: 'JobBoard',
+      href: '/pek2024/jobboard',
     },
     {
       text: 'Staff',
@@ -42,4 +42,42 @@ export const footerData = {
   footNote: `
     Copyright &copy; 一般社団法人クラウドネイティブイノベーターズ協会
   `,
+};
+
+// スポンサーデータ
+export const sponsorsData = {
+  Diamond: [
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+  ],
+  Platinum: [
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+  ],
+  Gold: [
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+  ],
+  Silver: [
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+  ],
+  Bronze: [
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+    { name: 'XXX', src: import('~/assets/images/pek2024/pek2024_holizontal.png'), href: 'https://www.cnia.io/' },
+  ],
+};
+
+// スポンサーランクごとのグリッド設定
+export const rankGridSettings = {
+  Diamond: { default: '1', md: '1' },
+  Platinum: { default: '1', md: '2' },
+  Gold: { default: '2', md: '2' },
+  Silver: { default: '2', md: '4' },
+  Bronze: { default: '3', md: '4' },
 };
