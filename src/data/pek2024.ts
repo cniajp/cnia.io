5c1fc9db9ac95d0fa7278f74aa1508360e8ec04c
tanayan299 <infuse130@gmail.com>
2024-03-10 12:59:54 +0900

    feat: update each session page

---
+++ src/data/pek2024.ts
@@ -0,0 +1,23 @@
+export const sessionsList = [
+  {
+    id: 999,
+    title: 'タイトル',
+    abstract: '概要\r\n\r\n概要',
+    talkDifficulty: '初級者',
+    talkCategory: 'Culture',
+    videoUrl: '',
+    documentUrl: '',
+    speakers: [
+      {
+        name: 'Name',
+        imagePath: 'imapePathName.{jpeg,jpg,png,gif}',
+        company: 'Company Name',
+        jobTitle: 'Job Title',
+        profile: '登壇者プロフィール',
+        githubId: 'GitHub Id',
+        twitterId: 'Twitter Id',
+      },
+    ],
+    status: 'sample',
+  },
+];
