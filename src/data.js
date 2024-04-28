569ea85487d9c9b25a1ae898fd3e31b2a0d5f9ad
ishikawa-pro <ishikawa.pro@gmail.com>
2024-04-28 17:39:35 +0900

    feature: change past sessions list on PFEM's TOP

--- src/data.js
+++ src/data.js
@@ -24,7 +24,7 @@ export const headerData = {
         },
         {
           text: '過去セッション一覧',
-          href: '/pfem/past_events',
+          href: '/pfem/sessions',
         },
       ],
     },
