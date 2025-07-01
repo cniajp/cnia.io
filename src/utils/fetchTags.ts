ad1c8f0facaf8e82474e869197a7df8fab7e5708
kitsuyaazuma <kitsuyaazuma@gmail.com>
2025-07-02 08:09:30 +0900

    fix: cheerio does not provide an export named 'default'

--- src/utils/fetchTags.ts
+++ src/utils/fetchTags.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import type { ForteeTag } from '../types'; // タグの型定義をインポート
 
 export async function fetchTags(url: string): Promise<ForteeTag[]> {
